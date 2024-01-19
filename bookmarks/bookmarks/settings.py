86a9f018b4dc2d0f995170c37922643424d09a92
nickAnhel <127442031+nickAnhel@users.noreply.github.com>
2024-01-19 19:25:16 +0300

    Ch.4 Add authentication and password managing

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from .confidential_data import GOOGLE_EMAIL, GOOGLE_PASSWORD
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,3 +123,15 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# login settings
+LOGIN_REDIRECT_URL = "dashboard"
+LOGIN_URL = "login"
+LOGOUT_URL = "logout"
+
+# SMTP-server configuration (Simple Mail Transfer Protocol)
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = GOOGLE_EMAIL
+EMAIL_HOST_PASSWORD = GOOGLE_PASSWORD
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True  # TLS - Transport Layer Security
