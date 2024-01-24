5e7e2e2c4c09165fddd386e2045ddfe56900b1a0
nickAnhel <127442031+nickAnhel@users.noreply.github.com>
2024-01-24 20:13:28 +0300

    Ch.4 Add registration

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,9 +1,10 @@
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 
 
 def user_login(request: HttpRequest) -> HttpResponse:
@@ -33,3 +34,16 @@ def user_login(request: HttpRequest) -> HttpResponse:
 @login_required
 def dashboard(request: HttpRequest) -> HttpResponse:
     return render(request, "account/dashboard.html", {"section": "dashboard"})
+
+
+def register(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user: User = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data["password"])
+            new_user.save()
+            return render(request, "account/register_done.html", {"new_user": new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, "account/register.html", {"user_form": user_form})
