707f5c240421e35a4b8e453b943074b395ff533a
nickAnhel <127442031+nickAnhel@users.noreply.github.com>
2024-02-05 00:17:01 +0300

    Ch.5 Add social authentication with google account

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 from environs import Env
 
-from .confidential_data import GOOGLE_EMAIL, GOOGLE_PASSWORD
-
 
 env = Env()
 env.read_env()
@@ -32,7 +30,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["mysite.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -45,6 +43,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "social_django",
+    "django_extensions",
 ]
 
 MIDDLEWARE = [
@@ -78,8 +78,22 @@ TEMPLATES = [
 WSGI_APPLICATION = "bookmarks.wsgi.application"
 
 AUTHENTICATION_BACKENDS = [
- 'django.contrib.auth.backends.ModelBackend',
- 'account.authentication.EmailAuthBackend',
+    "django.contrib.auth.backends.ModelBackend",
+    "account.authentication.EmailAuthBackend",
+    "social_core.backends.google.GoogleOAuth2",
+]
+
+SOCIAL_AUTH_PIPELINE = [
+    "social_core.pipeline.social_auth.social_details",
+    "social_core.pipeline.social_auth.social_uid",
+    "social_core.pipeline.social_auth.auth_allowed",
+    "social_core.pipeline.social_auth.social_user",
+    "social_core.pipeline.user.get_username",
+    "social_core.pipeline.user.create_user",
+    "account.authentication.create_profile",
+    "social_core.pipeline.social_auth.associate_user",
+    "social_core.pipeline.social_auth.load_extra_data",
+    "social_core.pipeline.user.user_details",
 ]
 
 # Database
@@ -141,11 +155,15 @@ LOGOUT_URL = "logout"
 
 # SMTP-server configuration (Simple Mail Transfer Protocol)
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = GOOGLE_EMAIL
-EMAIL_HOST_PASSWORD = GOOGLE_PASSWORD
+EMAIL_HOST_USER = env("GOOGLE_EMAIL")
+EMAIL_HOST_PASSWORD = env("GOOGLE_PASSWORD")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True  # TLS - Transport Layer Security
 
 # Media settings
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
+
+# Google authentication settings
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = env("GOOGLE_OAUTH2_KEY")
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = env("GOOGLE_OAUTH2_SECRET")
