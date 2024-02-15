6b59348dd4000dbf24cf47b41e3cc9723286cb91
nickAnhel <anhimovn1@gmail.com>
2024-02-15 12:17:24 +0300

    Ch.7 Move project to wsl

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -46,9 +46,11 @@ INSTALLED_APPS = [
     "django_extensions",
     "easy_thumbnails",
     "actions.apps.ActionsConfig",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -56,6 +58,10 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+INTERNAL_IPS = [
+    "127.0.0.1",
 ]
 
 ROOT_URLCONF = "bookmarks.urls"
